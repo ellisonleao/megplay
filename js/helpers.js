824ee3e2f5372306d6fd83a4e6f1c57bd72b4f80
Ellison Leão <ellisonleao@gmail.com>
2013-06-15 08:32:06 -0300

    Refactor the core game / add succes or fail message on check collision

---
+++ js/helpers.js
@@ -0,0 +1,18 @@
+Number.prototype.clamp = function(min, max) {
+  return Math.min(Math.max(this, min), max);
+};
+Object.size = function(obj) {
+    var size = 0, key;
+    for (key in obj) {
+        if (obj.hasOwnProperty(key)) size++;
+    }
+    return size;
+};
+// Array Remove - By John Resig (MIT Licensed)
+Array.prototype.remove = function(from, to) {
+  var rest = this.slice((to || from) + 1 || this.length);
+  this.length = from < 0 ? this.length + from : from;
+  return this.push.apply(this, rest);
+};
+
+
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var connect = require('connect');
+console.log('listening..')
 connect.createServer(
     connect.static(__dirname)
 ).listen(8080);
